d16e88ac8bb87ffb279d0a75ab1e4d9fb752473e
Caitlin Lindauer <clilos018@gmail.com>
2022-03-30 13:33:38 -0600

    addd if and else statments, and a while loop

--- script.js
+++ script.js
@@ -1,44 +1,55 @@
 //Start server side options
 var playerchoice = "";
 var playerchoiceStr;
+var wantstoplay = false
 
-alert("Would you like to play a game")
+wantstoplay = confirm("Would you like to play a game");
+while (wantstoplay){
+    var correctResponsesArray = ["R", "r", "P", "p", "S", "s"];
 
-var correctResponsesArray = ["R", "r", "P", "p", "S", "s"];
+    //here we have a while loop with the condition that while
+    //playerchoice is not equal to anything inside the correct
+    //response array, we keep prompting the user to do it correctly
+    while (!correctResponsesArray.includes(playerchoice)) {
+        playerchoice = prompt("Rock, Paper, Scissors.\nType R, P, S to play.")
+    }
 
-//here we have a while loop with the condition that while
-//playerchoice is not equal to anything inside the correct
-//response array, we keep prompting the user to do it correctly
-while (!correctResponsesArray.includes(playerchoice)) {
-    playerchoice = prompt("Rock, Paper, Scissors.\nType R, P, S to play.")
+
+    //now we want to make our playerchoiceStr the full word
+    if (playerchoice === 'R' || playerchoice === 'r') {
+        playerchoiceStr = "rock";
+    } else if (playerchoice === 'P' || playerchoice === 'p') {
+        playerchoiceStr = 'paper';
+    } else {
+        playerchoiceStr = 'scissors';
+    } 
+
+    //Computer side
+    var compchoice;
+    var compchoiceStr;
+
+    //with the Math.floor(Math.random() * (3)
+    //we have set up a non-inclusive maximum for the number randomizer
+    //so that means this can only result in 0 1 or 2
+    compchoice = Math.floor(Math.random() * (3))
+
+    //now we want to make our compchoiceStr either rock paper or scissors
+    //depending on what number we got above
+    if (compchoice === 0) {
+        compchoiceStr = "rock";
+    } else if (compchoice === 1) {
+        compchoiceStr = "paper";
+    } else {
+        compchoiceStr = "scissors";
+    }
+    console.log(compchoiceStr);
+
+    //
+    var win
+    var lost
+    var tie
+
+    if (playerchoiceStr === "rock" && compchoiceStr === "scissors") {
+
+    }
 }
-
-
-//now we want to make our playerchoiceStr the full word
-if (playerchoice === 'R' || playerchoice === 'r') {
-    playerchoiceStr = "rock";
-} else if (playerchoice === 'P' || playerchoice === 'p') {
-    playerchoiceStr = 'paper';
-} else {
-    playerchoiceStr = 'scissors';
-} 
-
-//Computer side
-var compchoice;
-var compchoiceStr;
-
-//with the Math.floor(Math.random() * (3)
-//we have set up a non-inclusive maximum for the number randomizer
-//so that means this can only result in 0 1 or 2
-compchoice = Math.floor(Math.random() * (3))
-
-//now we want to make our compchoiceStr either rock paper or scissors
-//depending on what number we got above
-if (compchoice === 0) {
-    compchoiceStr = "rock";
-} else if (compchoice === 1) {
-    compchoiceStr = "paper";
-} else {
-    compchoiceStr = "scissors";
-}
-console.log(compchoiceStr);
